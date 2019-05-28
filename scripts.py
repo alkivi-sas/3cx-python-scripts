fa44995d0153fcf300d60e8682a7e991845bbcbc
Anthony Martinet <anthony@alkivi.fr>
2019-05-28 16:30:41 +0200

    fix(order)

--- scripts.py
+++ scripts.py
@@ -119,8 +119,8 @@ def check_3cx_data(debug):
     # Parameter check
     parameters_to_check = {
             'E164': '0',
-            'MS_LOCAL_CODEC_LIST': 'PCMU G729 PCMA G722 GSM OPUS',
-            'MS_EXTERNAL_CODEC_LIST': 'PCMU G729 PCMA G722 GSM OPUS',
+            'MS_LOCAL_CODEC_LIST': 'PCMA G729 PCMU G722 GSM OPUS',
+            'MS_EXTERNAL_CODEC_LIST': 'PCMA G729 PCMU G722 GSM OPUS',
     }
     logger.new_loop_logger()
     for name, value in parameters_to_check.items():
